30958293376e2e2d623afd2544c76f0c8445f712
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-04 15:35:03 +0100

    Bonus Esercizio 1

--- Bonus/JS/script.js
+++ Bonus/JS/script.js
@@ -2,6 +2,23 @@
 L’utente inserisce due parole in successione, con due prompt. 
 Il software stampa prima la parola più corta, poi la parola più lunga; */
 
+//l'utente inserisce la prima parola
+const primaParola = prompt('Inserisci la prima parola');
+
+//l'utente inserisce la seconda parola
+const secondaParola = prompt('Inserisci la seconda parola');
+
+//andiamo a virificare quale parola sia più corta
+
+if (primaParola.length > secondaParola.length){
+    console.log(secondaParola, primaParola);
+}
+else if (primaParola.length < secondaParola.length){
+    console.log(primaParola, secondaParola );
+}
+else{
+    console.log('Le parole hanno la stessa lunghezza e te le metto in ordine di input:', primaParola, secondaParola)
+}
 
 
 
