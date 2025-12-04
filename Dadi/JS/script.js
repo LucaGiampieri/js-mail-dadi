3818d894153c26ee4696ac350c45749b48c722e4
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-04 15:24:18 +0100

    Esercizio Dadi

--- Dadi/JS/script.js
+++ Dadi/JS/script.js
@@ -5,3 +5,31 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
+
+//facciamo creare un numero casuale per il giocatore
+const numGiocatore = Math.floor(Math.random() * 6) + 1;
+console.log('Numero Giocatore:',numGiocatore);
+
+//facciamo creare un numero casuale per il computer
+const numPC = Math.floor(Math.random() * 6) + 1;
+console.log('Numero PC:',numPC);
+
+//facciamo comparare i due numeri e vediamo chi è il vicitore
+
+risultato = '';
+
+//se il giocatora ha il numero più alto
+if (numGiocatore > numPC){
+    risultato = 'Il giocatore è il vincitore!';
+}
+//se il Pc ha il ha il numero più alto
+else if (numGiocatore < numPC){
+    risultato = 'Il Pc è il vincitore!';
+}
+//se sono pari
+else {
+    risultato =
+    'La partita finisce in parità!';
+}
+
+console.log(risultato);
