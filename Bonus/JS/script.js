0f9aa8e90ac19115dcf7d813c710ff278c022cb1
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-04 14:43:41 +0100

    feat all index.html and all script.js

---
+++ Bonus/JS/script.js
@@ -0,0 +1,13 @@
+/* JSnack 2
+L’utente inserisce due parole in successione, con due prompt. 
+Il software stampa prima la parola più corta, poi la parola più lunga; */
+
+
+
+
+
+
+/* JSnack 5
+Crea un array vuoto. 
+Chiedi per 6 volte all’utente di inserire un numero, 
+se è dispari inseriscilo nell’array */
